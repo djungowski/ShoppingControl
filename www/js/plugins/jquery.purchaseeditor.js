b963a7d093499b5f07e40118dc1de5cb69978183
Dominik Jungowski <Dominik@Jungowski.de>
2012-09-19 23:48:11 +0200

    More buttons, show checkboxes dynamically, etc.

---
+++ www/js/plugins/jquery.purchaseeditor.js
@@ -0,0 +1,45 @@
+(function(jQuery, $) {
+    $.fn.purchaseeditor = function(options) {
+        options = $.extend($.fn.purchaseeditor.options, options);
+        
+        this.find('.edit').each(function(key, item) {
+            $(item).bind('click', $.fn.purchaseeditor.editHandler);
+        });
+        
+        this.find('.cancel').each(function(key, item) {
+            $(item).bind('click', $.fn.purchaseeditor.cancelHandler);
+        });
+        
+        this.find('.delete').each(function(key, item) {
+            $(item).bind('click', $.fn.purchaseeditor.deleteHandler);
+        });
+    };
+    
+    $.fn.purchaseeditor.editHandler = function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        $('.delete-purchase').show();
+        $('.edit-purchases-buttons .edit').hide();
+        $('.edit-purchases-buttons .delete').show();
+        $('.edit-purchases-buttons .cancel').show();
+    };
+    
+    $.fn.purchaseeditor.cancelHandler = function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        $('.delete-purchase').hide();
+        $('.edit-purchases-buttons .edit').show();
+        $('.edit-purchases-buttons .delete').hide();
+        $('.edit-purchases-buttons .cancel').hide();
+    };
+    
+    $.fn.purchaseeditor.deleteHandler = function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        // @TODO: Implement form submission here
+    };
+    
+    $.fn.purchaseeditor.options = {
+            
+    };
+})(jQuery, jQuery);
